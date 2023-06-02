13941a97b46327cd228586dfbb5fedb1b7f495ab
MajewskiL <lukasz.majewski@tlen.pl>
2023-06-02 21:44:06 +0200

    Created project structure and files.

---
+++ X_Mass_Tree/stage2/tests.py
@@ -0,0 +1,4 @@
+from test.tests import XMassTreeTest2
+
+if __name__ == '__main__':
+    XMassTreeTest2().run_tests()
---
+++ X_Mass_Tree/stage4/test/tests.py
@@ -0,0 +1,23 @@
+from hstest import StageTest, TestCase, CheckResult
+from hstest.stage_test import List
+
+class XMassTreeTest4(StageTest):
+
+#    def generate(self) -> List[TestCase]:
+#        return [TestCase(time_limit=1000000)]#
+
+    def check(self, reply: str):
+
+        if len(reply) == 0:
+            return CheckResult.wrong("No output was printed!")
+
+        #reply = float(reply)
+        #tolerance = 0.1
+
+        # Getting the student's results from the reply
+
+        #if tolerance:
+        #    if not (abs((reply - true_data) / true_data) < tolerance):
+        #        return CheckResult.wrong('Incorrect value.')
+
+        return CheckResult.correct()
