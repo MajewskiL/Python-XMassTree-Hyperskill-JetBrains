b20b4b77053420bce9dbbdc1c7291c4f9add8762
MajewskiL <lukasz.majewski@tlen.pl>
2023-06-03 14:59:05 +0200

    Created tests for stage 1.

--- X_Mass_Tree/stage1/test/tests.py
+++ X_Mass_Tree/stage1/test/tests.py
@@ -1,20 +1,53 @@
-from hstest import StageTest, TestCase, CheckResult
+from hstest import StageTest, CheckResult, dynamic_test, TestedProgram
 from hstest.stage_test import List
-
-#  !!!!!!!!!!!!!!!!!!
-#  Ilość linii, start gwiazdek w każdej (ale nie ogólnie tylko rosnąco)
-#  i suma gwiazdek.
+from random import randint
 
 
 class XMassTreeTest1(StageTest):
 
-#    def generate(self) -> List[TestCase]:
-#        return [TestCase(time_limit=1000000)]#
+    @staticmethod
+    def output_len_stage1(out, high):
+        out_len = len(out.splitlines())
+        if out_len != int(high):
+            return f"Wrong tree high. Expected {high}, founded {out_len}."
 
-    def check(self, reply: str):
+    @staticmethod
+    def output_stars_stage1(out, high):
+        out_stars = out.count("*")
+        exp_stars = sum([2 * n + 1 for n in range(int(high))])
+        if out_stars != exp_stars:
+            return f"Wrong number of '*'. Expected {exp_stars}, founded {out_stars}."
+        return
 
-        if len(reply) == 0:
-            return CheckResult.wrong("No output was printed!")
+    @staticmethod
+    def output_pos_stage1(out, high):
+        out = out.splitlines()
+        out_pos = [n.index("*") for n in out]
+        exp_pos = [int(high - n - 1) for n in range(high)]
+        for i, value in enumerate(out_pos):
+            if value != exp_pos[i]:
+                return f"Wrong position of '*' in line {i}. Expected {exp_pos[i]}, founded {value}."
+        return
+
+    @dynamic_test
+    def test1(self):
+        high = str(randint(3, 30))
+        # high = '3'
+        main = TestedProgram()
+        main.start()
+        output = main.execute(high)
+        check = self.output_stars_stage1(output, high)
+        if check:
+            return CheckResult.wrong(check)
+        check = self.output_len_stage1(output, high)
+        if check:
+            return CheckResult.wrong(check)
+        check = self.output_pos_stage1(output, int(high))
+        if check:
+            return CheckResult.wrong(check)
+
+#    if len(reply) == 0:
+#        return CheckResult.wrong("No output was printed!")
 
         #reply = float(reply)
         #tolerance = 0.1
