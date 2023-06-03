0de991f2c9c19fe7202a9b1f8184c519ad97a0ed
MajewskiL <lukasz.majewski@tlen.pl>
2023-06-03 20:39:50 +0200

    Added tests for stage 2.

--- X_Mass_Tree/stage2/test/tests.py
+++ X_Mass_Tree/stage2/test/tests.py
@@ -1,28 +1,70 @@
-from hstest import StageTest, TestCase, CheckResult
-from hstest.stage_test import List
-
 #  !!!!!!!!!!!!!!!!!!!!!!
 #  Algorytm gdzie mam być czubek, czubek choinki, pień, pierwszy i ostatni listek
 #  na jakiej pozycja dla całej choinki, czyli pary koordynatów dla każdej
 #  linii i suma gwiazdek!
+from hstest import StageTest, CheckResult, dynamic_test, TestedProgram
+from random import randint
+
 
 class XMassTreeTest2(StageTest):
 
-#    def generate(self) -> List[TestCase]:
-#        return [TestCase(time_limit=1000000)]#
+    @staticmethod
+    def output_len_stage1(out, high):
+        out_len = len(out.splitlines())
+        if out_len != int(high) + 2:
+            return f"Wrong tree high. Expected {high}, founded {out_len}."
+        return
 
-    def check(self, reply: str):
+    @staticmethod
+    def output_ext_stage2(out, high):
+        out = out.splitlines()
+        ext = [("X", 0, 0), ("^", 1, 0), ("I I", len(out) - 1, -1)]
+        for item, i, correction in ext:
+            out_pos = [out[i].index(item) if item in out[i] else None,
+                       out[i].count(item),
+                       len(out[i].strip())]
+            exp_pos = [int(high - 1) + correction, 1, len(item)]
+            if out_pos[0] != exp_pos[0]:
+                return f"Wrong position of {item} in line {i}. Expected {exp_pos[0]}, founded {out_pos[0]}."
+            if out_pos[1] != exp_pos[1]:
+                return f"Wrong number of {item} in line {i}. Expected {exp_pos[1]}, founded {out_pos[1]}."
+            if out_pos[2] != exp_pos[2]:
+                return f"Wrong width of the tree in line {i}. " \
+                       f"Expected {exp_pos[2]} chars, founded {out_pos[2]} chars."
+        return
 
-        if len(reply) == 0:
-            return CheckResult.wrong("No output was printed!")
+    @staticmethod
+    def output_pos_stage2(out, high):
+        out = out.splitlines()[2:-1]
+        #  extra condition "ZONK" for empty line to return None
+        out_pos = [[n.index(n.strip()) if (n.strip() or "ZONK") in n else None, n.count("*"), len(n.strip()), n.strip()] for n in out]
+        exp_pos = [[int(high - n - 1), 2 * n + 1 - 2, 2 * n + 1] for n in range(1, high)]
+        for i, value in enumerate(out_pos):
+            if value[0] != exp_pos[i][0]:
+                return f"Wrong position of line {i + 3}. Expected {exp_pos[i][0]}, founded {value[0]}."
+            if value[1] != exp_pos[i][1]:
+                return f"Wrong number of '*' in line {i + 3}. Expected {exp_pos[i][1]}, founded {value[1]}."
+            if value[2] != exp_pos[i][2]:
+                return f"Wrong width of the tree in line {i + 3}. Expected {exp_pos[i][2]} chars, founded {value[2]} chars."
+            if not value[3].endswith("\\"):
+                return f"Tree in line {i + 3} doesn't ends with '\\'. Expected '\\', founded {value[3][-1]}."
+            if not value[3].startswith("/"):
+                return f"Tree in line {i + 3} doesn't starts with '/'. Expected '/', founded {value[3][0]}."
+        return
 
-        #reply = float(reply)
-        #tolerance = 0.1
-
-        # Getting the student's results from the reply
-
-        #if tolerance:
-        #    if not (abs((reply - true_data) / true_data) < tolerance):
-        #        return CheckResult.wrong('Incorrect value.')
+    @dynamic_test
+    def test1(self):
+        for _ in range(3):
+            main = TestedProgram()
+            main.start()
+            high = str(randint(3, 30))
+            output = main.execute(high)
+            func = [self.output_len_stage1(output, high),
+                    self.output_ext_stage2(output, int(high)),
+                    self.output_pos_stage2(output, int(high))]
+            for f in func:
+                check = f
+                if check:
+                    return CheckResult.wrong(check)
 
         return CheckResult.correct()
